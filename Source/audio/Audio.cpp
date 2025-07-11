9c2e904f0772b08db08d644001b06266e01256e1
cford <c.ford@arts.ac.uk>
2025-07-11 16:14:11 +0100

    Made so will build

--- Source/audio/Audio.cpp
+++ Source/audio/Audio.cpp
@@ -21,13 +21,14 @@ namespace codetta
         audioSourcePlayer.setSource (&codettaAudioSource);
         
         // add the audio callback
-        audioDeviceManager.addAudioCallback (this);
+//        audioDeviceManager.addAudioCallback (this);
+        audioDeviceManager.addAudioCallback(&audioSourcePlayer);
     }
     
     Audio::~Audio()
     {
         audioSourcePlayer.setSource (nullptr);
-        audioDeviceManager.removeAudioCallback (this);
+//        audioDeviceManager.removeAudioCallback (this);
     }
     
     //======================================================================
@@ -45,62 +46,62 @@ namespace codetta
     
     //==========================================================================
 
-    void Audio::audioDeviceAboutToStart (AudioIODevice* device)
-    {
-        audioSourcePlayer.audioDeviceAboutToStart (device);
-        reverb.setSampleRate (device->getCurrentSampleRate());
-        
-        Reverb::Parameters params = reverb.getParameters();
-        params.wetLevel = 0.8f;
-        params.damping = 0.5f;
-        params.width = 0.8f;
-        params.dryLevel = 0.6f;
-        params.roomSize = 0.3f;
-        reverb.setParameters (params);
-    }
-    
-    void Audio::audioDeviceIOCallback (const float** inputChannelData,
-                                       int numInputChannels,
-                                       float** outputChannelData,
-                                       int numOutputChannels,
-                                       int numSamples)
-    {
-        audioSourcePlayer.audioDeviceIOCallback (inputChannelData,
-                                                 numInputChannels,
-                                                 outputChannelData,
-                                                 numOutputChannels,
-                                                 numSamples);
-
-        auto outLeft = outputChannelData[0];
-        auto outRight = outputChannelData[1];
-        const float outGain = 0.010;
-        
-        // Buffer rate block...
-        if (outRight == nullptr) // iphones and the like are mono
-            reverb.processMono (outLeft, numSamples);
-        else
-            reverb.processStereo (outLeft, outRight, numSamples);
-
-
-        // Sample rate block...
-        while (numSamples--)
-        {
-            *outLeft *= outGain;
-            outLeft++;
-            
-            if (outRight != nullptr) // iphones and the like are mono
-            {
-                *outRight *= outGain;
-                outRight++;
-            }
-        }
-        
-    }
-    
-    void Audio::audioDeviceStopped()
-    {
-        reverb.reset();
-        audioSourcePlayer.audioDeviceStopped();
-    }
+//    void Audio::audioDeviceAboutToStart (AudioIODevice* device)
+//    {
+//        audioSourcePlayer.audioDeviceAboutToStart (device);
+//        reverb.setSampleRate (device->getCurrentSampleRate());
+//        
+//        Reverb::Parameters params = reverb.getParameters();
+//        params.wetLevel = 0.8f;
+//        params.damping = 0.5f;
+//        params.width = 0.8f;
+//        params.dryLevel = 0.6f;
+//        params.roomSize = 0.3f;
+//        reverb.setParameters (params);
+//    }
+//    
+//    void Audio::audioDeviceIOCallback (const float** inputChannelData,
+//                                       int numInputChannels,
+//                                       float** outputChannelData,
+//                                       int numOutputChannels,
+//                                       int numSamples)
+//    {
+////        audioSourcePlayer.audioDeviceIOCallback (inputChannelData,
+////                                                 numInputChannels,
+////                                                 outputChannelData,
+////                                                 numOutputChannels,
+////                                                 numSamples);
+//
+//        auto outLeft = outputChannelData[0];
+//        auto outRight = outputChannelData[1];
+//        const float outGain = 0.010;
+//        
+//        // Buffer rate block...
+//        if (outRight == nullptr) // iphones and the like are mono
+//            reverb.processMono (outLeft, numSamples);
+//        else
+//            reverb.processStereo (outLeft, outRight, numSamples);
+//
+//
+//        // Sample rate block...
+//        while (numSamples--)
+//        {
+//            *outLeft *= outGain;
+//            outLeft++;
+//            
+//            if (outRight != nullptr) // iphones and the like are mono
+//            {
+//                *outRight *= outGain;
+//                outRight++;
+//            }
+//        }
+//        
+//    }
+//    
+//    void Audio::audioDeviceStopped()
+//    {
+//        reverb.reset();
+//        audioSourcePlayer.audioDeviceStopped();
+//    }
     
 } // namespace codetta
